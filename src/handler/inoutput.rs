0924e329c6624449c803b06e15dd990ae457f9c5
shanliu <shanliu@msn.com>
2021-01-04 18:40:50 +0800

    ...

--- src/handler/inoutput.rs
+++ src/handler/inoutput.rs
@@ -1,33 +1,82 @@
 use actix_web::{get, web, HttpResponse};
 use crate::SiteSetting;
 use serde::{Deserialize, Serialize};
-use sqlx::mysql::MySqlPoolOptions;
+use actix_web::{error, Result,error::ResponseError};
+use derive_more::{Display, Error};
+use serde_json::json;
+use sqlx::mysql::MySqlRow;
+use sqlx::{Row, Error};
+use actix_redis::Command;
+use redis_async::{resp::RespValue, resp_array};
 
 
-#[get("/")]
-pub(crate) async fn index(data: web::Data<SiteSetting>) -> String {
-    let app_name = &data.app_name;
+#[derive(Debug, Display, Error)]
+#[display(fmt = "my error: {}", name)]
+pub struct WebHandError{
+    name: String,
+}
+impl ResponseError for WebHandError{
 
-
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&data.db).await.unwrap();
-
-    assert_eq!(row.0, 150);
-
-    format!("Hello {}!", app_name)
+}
+impl From<sqlx::Error> for WebHandError{
+    fn from(err: sqlx::Error) -> Self {
+        let b=err.as_database_error().unwrap().message().to_string();
+        return WebHandError{
+            name:b
+        }
+    }
 }
 
 
-#[get("/show/{cat}/{id?}")]
-pub(crate) async fn user_detail(web::Path((cat,id)):web::Path<(String, i32)>) -> HttpResponse {
-    #[derive(Debug, Serialize, Deserialize)]
-    struct MyObj {
-        cat: String,
-        id: i32,
-    }
-    HttpResponse::Ok().json(MyObj{
-        cat:cat,
-        id:id
-    })
+#[derive(Serialize, Deserialize)]
+struct MyObj {
+    name: String,
+}
+#[derive(Deserialize)]
+pub struct AuthRequest {
+   id: u64,
+   name: String,
+}
+
+
+#[get("/")]
+pub(crate) async fn index(web::Query(info):web::Query<AuthRequest>,data: web::Data<SiteSetting>) -> Result<HttpResponse,WebHandError> {
+    let app_name = &data.app_name;
+
+    let row = sqlx::query_as("SELECT Fid,Frule_data from t_pm_valid_data_rule where Fid>=?")
+        .bind(info.id as u32)
+        .fetch_one(&data.db).await;
+    let brow:(u32,String)=row.map_err(|e|{
+        match e {
+            Error::Database(err) =>{
+                let b=err.message().to_string();
+                WebHandError{name:b}
+            },
+            Error::RowNotFound=>{
+                return WebHandError{name:"无记录".to_string()};
+            }
+            x@_ => {
+                let b:String=format!("{:?}", x);
+                return WebHandError{name:b};
+            },
+        }
+    })?;
+
+    Ok(HttpResponse::Ok().json(MyObj {
+        name: format!("Hello {} {}!", brow.0,brow.1)
+    }))
+}
+
+
+#[get("/show/{cat}/{id}")]
+pub(crate) async fn user_detail(web::Path((cat,id)):web::Path<(String, i32)>,data: web::Data<SiteSetting>) -> Result<HttpResponse,WebHandError> {
+
+    let one = data.redis.send(Command(resp_array!["SET", "mydomain:one", "info.one"]));
+    let b=one.await;
+
+
+    Ok(HttpResponse::Ok().json(json!({
+        "cat":cat,
+        "id":id
+    })))
 }
--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,18 @@ use actix_web::{
 }};
 
 use actix_files as fs;
-use sqlx::{MySqlPool, MySql, Pool, Database};
+use sqlx::{ MySql, Pool};
 use sqlx::pool::PoolOptions;
-use sqlx::mysql::MySqlConnectOptions;
-use futures::task::Poll;
-use std::sync::Arc;
+use actix_redis::RedisActor;
+use actix::Addr;
+
 
 mod handler;
 
 struct SiteSetting {
     app_name: String,
-    db:Pool<MySql>
+    db:Pool<MySql>,
+    redis:Addr<RedisActor>
 }
 
 async fn p404() -> Result<fs::NamedFile> {
@@ -32,27 +33,22 @@ async fn p404() -> Result<fs::NamedFile> {
 async fn main() -> std::io::Result<()> {
     let pool = PoolOptions::<MySql>::new()
         .max_connections(5)
-        .connect("mysql://root:123456@127.0.0.1/database").await.unwrap();
-
+        .connect("mysql://root:@127.0.0.1/test").await.unwrap();
+    let redis_addr = RedisActor::start("127.0.0.1:6379");
     std::env::set_var("RUST_LOG", "actix_web=info");
     HttpServer::new(move|| {
         App::new()
             .wrap(middleware::Logger::default())
             .data(SiteSetting {
                 app_name: String::from("Actix-web"),
-                db:pool.clone()
+                db:pool.clone(),
+                redis:redis_addr.clone()
             })
             .service(
             handler::inoutput::index
             )
-            // .service(
-            // handler::inoutput::user_detail
-            // )
             .service(
-            web::resource("/test/{c}")
-                     .name("foo") // <- set resource name, then it could be used in `url_for`
-                     .guard(guard::Post())
-                     .to(|| HttpResponse::Ok().body("dddddddddddddd"))
+            handler::inoutput::user_detail
             )
             .default_service(
                 web::resource("")
